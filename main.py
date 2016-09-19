950a1a48d56f5ab052b075c88aff95431aba1a62
sidney <sidney.nakatani@hotmail.com>
2016-09-19 00:35:17 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello World</hi>"
+
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
